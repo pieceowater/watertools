58b4a3cf9cd55f434208ed0e65cb0706e3c48536
pieceowater <yury.lvn@mail.ru>
2023-06-25 22:44:00 +0600

    fix

--- Sources/watertools/watertools.swift
+++ Sources/watertools/watertools.swift
@@ -1,121 +1,115 @@
 import SwiftUI
-import UIKit
 
 public struct OfflineAdBanner: View {
     public let ad: Ad = MyApps.randomElement()!
-    @State public var counter = 3
+    @State public var counter = 30
     @State public var timer: Timer?
     @State public var closeAllowed: Bool = false
     @Environment(\.presentationMode) public var presentationMode
-
-  
+    public init() {}
     public var body: some View {
-            ScrollView {
-                VStack {
-                    HStack{
-                        Image(ad.icon)
-                            .resizable()
+        ScrollView {
+            VStack {
+                HStack {
+                    Image(ad.icon)
+                        .resizable()
+                        .foregroundColor(.accentColor)
+                        .frame(width: 60, height: 60)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(15)
+                    VStack(alignment: .leading) {
+                        Text(ad.name)
+                            .font(.title)
                             .foregroundColor(.accentColor)
-                            .frame(width: 60, height: 60)
-//                            .background(Color.gray.opacity(0.3))
-                            .background(.ultraThinMaterial)
+                            .fontWeight(.bold)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.9)
+                        Text(ad.subtitle)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.8)
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    
+                    Button(action: {
+                        openAppStore()
+                    }, label: {
+                        Text("Get App")
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color.accentColor)
                             .cornerRadius(15)
-                        VStack(alignment: .leading) {
-                            Text(ad.name)
-                                .font(.title)
-                                .foregroundColor(.accentColor)
-                                .fontWeight(.bold)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.9)
-                            Text(ad.subtitle)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.8)
-                                .font(.subheadline)
+                    })
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                
+                HStack {
+                    Text(ad.caption)
+                        .font(.caption)
+                        .padding()
+                        .multilineTextAlignment(.leading)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        closeAd()
+                    }, label: {
+                        HStack(spacing: 10) {
+                            Text("Skip Ad")
+                            if closeAllowed {
+                                Image(systemName: "arrow.right.circle")
+                            } else {
+                                Text(String(counter))
+                                    .onAppear {
+                                        startTimer()
+                                    }
+                            }
                         }
-                        Spacer()
-                        
-                        Button(action: {
-                            openAppStore()
-                        }, label: {
-                            Text("Get App")
-                                .foregroundColor(.white)
-                                .padding(10)
-                                .background(Color.accentColor)
-                                .cornerRadius(15)
-                        })
+                        .foregroundColor(.primary)
+                        .padding(10)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(15)
+                        .font(.caption)
+                    })
+                    .disabled(!closeAllowed)
+                    .padding()
+                }
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(ad.previews, id: \.self) { previewImage in
+                            Image(previewImage)
+                                .resizable()
+                                .frame(width: 250, height: 500)
+                                .cornerRadius(30)
+                        }
                     }
                     .padding()
-//                    .background(Color.gray.opacity(0.1))
-                    .background(.ultraThinMaterial)
-                    .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    
-                    HStack{
-                        Text(ad.caption)
-                            .font(.caption)
-                            .padding()
-                            .multilineTextAlignment(.leading)
-                        
+                }
+                
+                Text(ad.description)
+                    .font(.caption)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                
+                Button {
+                    openAppStore()
+                } label: {
+                    HStack {
                         Spacer()
-                        
-                        Button(action: {
-                            closeAd()
-                        }, label: {
-                            HStack(spacing: 10){
-                                Text("Skip Ad")
-                                if closeAllowed {
-                                    Image(systemName: "arrow.right.circle")
-                                } else {
-                                    Text(String(counter))
-                                        .onAppear {
-                                            startTimer()
-                                        }
-                                }
-                            }
-                            .foregroundColor(.primary)
-                            .padding(10)
-//                            .background(Color.gray.opacity(0.3))
-                            .background(.ultraThinMaterial)
-                            .cornerRadius(15)
-                            .font(.caption)
-                        })
-                        .disabled(!closeAllowed)
-                        .padding()
+                        Text("Get App")
+                        Spacer()
                     }
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack{
-                            ForEach(ad.previews, id: \.self) { previewImage in
-                                Image(previewImage)
-                                    .resizable()
-                                    .frame(width: 250, height: 500)
-                                    .cornerRadius(30)
-                            }
-                        }.padding()
-                    }
-                    
-                    Text(ad.description)
-                        .font(.caption)
-                        .multilineTextAlignment(.leading)
-                        .padding()
-                    
-                    Button {
-                        openAppStore()
-                    } label: {
-                        HStack{
-                            Spacer()
-                            Text("Get App")
-                            Spacer()
-                        }
-                        .foregroundColor(.white)
-                        .padding(10)
-                        .background(Color.accentColor)
-                        .cornerRadius(15)
-                        .padding()
-                    }
-
-                    
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color.accentColor)
+                    .cornerRadius(15)
+                    .padding()
                 }
             }
+        }
     }
     
     private func openAppStore() {
@@ -126,24 +120,23 @@ public struct OfflineAdBanner: View {
     }
     
     private func startTimer() {
-            guard timer == nil else { return }
-            
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                if counter > 0 {
-                    counter -= 1
-                } else {
-                    timer?.invalidate()
-                    timer = nil
-                    closeAllowed = true
-                }
+        guard timer == nil else { return }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if counter > 0 {
+                counter -= 1
+            } else {
+                timer?.invalidate()
+                timer = nil
+                closeAllowed = true
             }
         }
-        
-        private func closeAd() {
-            print("AD CLOSED")
-            presentationMode.wrappedValue.dismiss()
-        }
+    }
     
+    private func closeAd() {
+        print("AD CLOSED")
+        presentationMode.wrappedValue.dismiss()
+    }
 }
 
 struct OfflineAdBanner_Previews: PreviewProvider {
