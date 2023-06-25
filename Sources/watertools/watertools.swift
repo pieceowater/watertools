c92286b32ba4843745089edf3f2a8612b0a2baba
pieceowater <yury.lvn@mail.ru>
2023-06-25 23:04:31 +0600

    fkc this shit

--- Sources/watertools/watertools.swift
+++ Sources/watertools/watertools.swift
@@ -45,7 +45,7 @@ public struct OfflineAdBanner: View {
                 }
                 .padding()
                 .background(.ultraThinMaterial)
-                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
                 
                 HStack {
                     Text(ad.caption)
