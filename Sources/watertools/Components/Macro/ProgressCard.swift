61d5be54bd30f6b83eace6b05d77f615e8182d13
pieceowater <yury.lvn@mail.ru>
2023-07-01 00:43:18 +0600

    new comps

---
+++ Sources/watertools/Components/Macro/ProgressCard.swift
@@ -0,0 +1,67 @@
+//
+//  ProgressCard.swift
+//  
+//
+//  Created by yury mid on 01.07.2023.
+//
+
+import SwiftUI
+
+public struct ProgressCard: View {
+    public let icon: Image
+    public let title: String
+    public let description: String
+    public let deadline: Date
+    public let checkedStepsCount: Int
+    public let commonStepsCount: Int
+    
+    init(icon: Image, title: String, description: String, deadline: Date, checkedStepsCount: Int, commonStepsCount: Int) {
+        self.icon = icon
+        self.title = title
+        self.description = description
+        self.deadline = deadline
+        self.checkedStepsCount = checkedStepsCount
+        self.commonStepsCount = commonStepsCount
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                icon
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 50)
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.top, 8)
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                    
+                    Text(description)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                    
+                }
+            }
+            HStack{
+                Image(systemName: "calendar.badge.clock")
+                Text(watertools.formattedDate(deadline))
+            }
+            .font(.caption)
+            .foregroundColor(.secondary)
+            .padding(.top, 8)
+            
+            ProgressTimeline(checkedStepsCount: checkedStepsCount, commonStepsCount: commonStepsCount)
+                .padding(.vertical, 10)
+            
+            HStack{
+                Spacer()
+            }
+        }
+    }
+    
+}
---
+++ Sources/watertools/Components/Micro/ProgressTimeline.swift
@@ -0,0 +1,36 @@
+//
+//  ProgressTimeline.swift
+//  
+//
+//  Created by yury mid on 01.07.2023.
+//
+
+import SwiftUI
+
+public struct ProgressTimeline: View {
+    public let checkedStepsCount: Int
+    public let commonStepsCount: Int
+    
+    init(checkedStepsCount: Int, commonStepsCount: Int) {
+        self.checkedStepsCount = checkedStepsCount
+        self.commonStepsCount = commonStepsCount
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Progress")
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                
+                HStack(alignment: .center, spacing: 4) {
+                    ForEach(0..<commonStepsCount, id: \.self) { index in
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(index < checkedStepsCount ? Color.accentColor : Color.gray.opacity(0.4))
+                            .frame(width: ((geometry.size.width / CGFloat(commonStepsCount)) - 4.0), height: 10)
+                    }
+                }
+            }
+        }
+    }
+}
