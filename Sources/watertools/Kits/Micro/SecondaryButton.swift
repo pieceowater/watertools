d7ac1e973f7b42d0f88165182d0715e5074b8ec4
pieceowater <yury.lvn@mail.ru>
2023-07-02 23:25:29 +0600

    SecondaryButton

---
+++ Sources/watertools/Kits/Micro/SecondaryButton.swift
@@ -0,0 +1,30 @@
+//
+//  SecondaryButton.swift
+//  
+//
+//  Created by yury mid on 02.07.2023.
+//
+
+import SwiftUI
+
+public struct SecondaryButton: View {
+    var action: () -> Void = { print("Warning: PrimaryButton Action is empty!") }
+    var text: String = "OK"
+    
+    public init(_ text: String, action: @escaping () -> Void){
+        self.text = text
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text(text)
+                .font(.subheadline)
+                .foregroundColor(.accentColor)
+                .padding()
+                
+        }
+    }
+}
