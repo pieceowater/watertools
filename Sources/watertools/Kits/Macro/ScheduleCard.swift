5f5718b8fe55f8d7aef02508b68f465695f56ce5
pieceowater <yury.lvn@mail.ru>
2023-07-03 17:02:08 +0600

    test

--- Sources/watertools/Kits/Macro/ScheduleCard.swift
+++ Sources/watertools/Kits/Macro/ScheduleCard.swift
@@ -18,24 +18,24 @@ public struct ScheduleCard<Destination:View>: View {
         let weekdays = calendar.weekdaySymbols
         return Array(weekdays.dropFirst() + weekdays.prefix(1).prefix(7))
     }()
-    var weekdayAction: (_ index: Int) -> Void
-    let weekdayContextMenuTitle: String
-    var weekdayContextMenuAction: (_ index: Int) -> Void
+    var weekdayAction: (_ index: Int) -> Void?
+    let weekdayContextMenuTitle: String?
+    var weekdayContextMenuAction: (_ index: Int) -> Void?
     let streak: Int
-    let destination: Destination
-    let navBtnText: String
+    let destination: Destination?
+    let navBtnText: String?
     
-    public init(icon: Image, title: String, comment: String, streak: Int = 0, weekdays: [Bool], weekdayAction: @escaping (_: Int) -> Void, weekdayContextMenuTitle: String, weekdayContextMenuAction: @escaping (_: Int) -> Void, destination: Destination, navBtnText: String) {
+    public init(icon: Image, title: String, comment: String, streak: Int = 0, weekdays: [Bool], weekdayAction: @escaping ((_: Int) -> Void)? = nil, weekdayContextMenuTitle: String? = nil, weekdayContextMenuAction: @escaping ((_: Int) -> Void)? = nil, destination: Destination? = nil, navBtnText: String? = nil) {
         self.icon = icon
         self.title = title
         self.comment = comment
         self.streak = streak
         self.weekdays = weekdays
-        self.weekdayAction = weekdayAction
-        self.weekdayContextMenuTitle = weekdayContextMenuTitle
-        self.weekdayContextMenuAction = weekdayContextMenuAction
-        self.destination = destination
-        self.navBtnText = navBtnText
+        self.weekdayAction = weekdayAction?
+        self.weekdayContextMenuTitle = weekdayContextMenuTitle?
+        self.weekdayContextMenuAction = weekdayContextMenuAction?
+        self.destination = destination?
+        self.navBtnText = navBtnText?
     }
     
     public var body: some View {
@@ -58,17 +58,19 @@ public struct ScheduleCard<Destination:View>: View {
                         .foregroundColor(.secondary)
                         .minimumScaleFactor(0.5)
                         .lineLimit(1)
-                    
-                    NavigationLink {
-                        destination
-                    } label: {
-                        HStack{
-                            Text(navBtnText)
-                            Image(systemName: "arrow.right")
+                    if let destination,
+                       navBtnText {
+                        NavigationLink {
+                            destination
+                        } label: {
+                            HStack{
+                                Text(navBtnText)
+                                Image(systemName: "arrow.right")
+                            }
+                            .font(.subheadline)
+                            .minimumScaleFactor(0.5)
+                            .lineLimit(1)
                         }
-                        .font(.subheadline)
-                        .minimumScaleFactor(0.5)
-                        .lineLimit(1)
                     }
                 }
                 Spacer()
@@ -86,29 +88,33 @@ public struct ScheduleCard<Destination:View>: View {
             
             HStack(spacing: 5) {
                 ForEach(0..<weekdays.count, id: \.self) { index in
-                    Button(action: {
-                        weekdayAction(index)
-                    }) {
-                        Text(weekdayNames[index].prefix(3))
-                            .font(.subheadline)
-                            .minimumScaleFactor(0.5)
-                            .lineLimit(1)
-                            .foregroundColor(weekdays[index] ? .accentColor : .gray)
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 4)
-                            .background(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .fill(weekdays[index] ? Color.accentColor.opacity(0.15) : Color.gray.opacity(0.15))
-                            )
-                    }
-                    .contextMenu {
-                        Button {
-                            weekdayContextMenuAction(index)
-                        } label: {
-                            Text(weekdayContextMenuTitle)
+                    
+                    Text(weekdayNames[index].prefix(3))
+                        .font(.subheadline)
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                        .foregroundColor(weekdays[index] ? .accentColor : .gray)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(weekdays[index] ? Color.accentColor.opacity(0.15) : Color.gray.opacity(0.15))
+                        )
+                        .onTapGesture {
+                            if let weekdayAction = weekdayAction {
+                                weekdayAction(index)
+                            }
                         }
-                        
-                    }
+                        .contextMenu {
+                            if let weekdayContextMenuTitle,
+                               let weekdayContextMenuAction {
+                                Button {
+                                    weekdayContextMenuAction(index)
+                                } label: {
+                                    Text(weekdayContextMenuTitle)
+                                }
+                            }
+                        }
                 }
             }
         }
